12d3ceda799178333ab44690ba040497ca161ab8
chabanova.es <chabanova.elis@gmail.com>
2026-07-16 13:10:22 +0300

    fix: checklist edits

--- src/components/profile-menu/profile-menu.tsx
+++ src/components/profile-menu/profile-menu.tsx
@@ -1,20 +1,19 @@
 import { FC } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { ProfileMenuUI } from '@ui';
-import { useDispatch, useSelector } from '../../services/store';
-import { selectError, userLogout } from '../../services/slices/userSlice';
+import { useDispatch } from '../../services/store';
+import { userLogout } from '../../services/slices/userSlice';
 
 export const ProfileMenu: FC = () => {
   const { pathname } = useLocation();
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const errorText = useSelector(selectError);
 
   const handleLogout = () => {
     dispatch(userLogout())
       .unwrap()
       .then(() => {
-        navigate('/');
+        navigate('/login');
       })
       .catch(() => {});
   };
--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -62,6 +62,4 @@ export const Profile: FC = () => {
       handleInputChange={handleInputChange}
     />
   );
-
-  return null;
 };
